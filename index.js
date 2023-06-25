48a49133c3f30b0c75522dbd774070386dd0f6f0
djamiranda <djamiranda@gmail.com>
2023-06-25 13:32:11 -0400

    updated write README & initizlize app functions

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const util = require("util");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -10,95 +9,71 @@ const questions = [
         type: "input",
         name: "Title",
         message: "Please enter the Project Title",
-      },
-    
+      },    
       {
         type: "input",
         name: "Description",
         message: "Please enter project description",
       },
-    
       {
         type: "input",
         name: "Installation",
         message: "Please enter installation steps",
       },
-    
       {
         type: "input",
         name: "Usage",
         message: "Please provide instructions & examples for use",
       },
-    
       {
         type: "list",
         name: "License",
         message: "Please choose license for project",
-        choices: [
-          {
-            name: "GNU General Public License v3.0",
+        choices: ["No License", "GNU General Public License v3.0", "MIT License", "Apache License 2.0"],
           },
-          {
-            name: "MIT License",
-          },
-          {
-            name: "Apache License 2.0",
-          },
-          {
-            name: "The Unilicense",
-          },
-        ],
-      },
-    
       {
         type: "input",
         name: "Contributing",
         message: "Please enter project contributors",
       },
-    
       {
         type: "input",
         name: "Tests",
         message: "Please enter project tests",
       },
-    
       {
         type: "input",
         name: "Email",
         message: "Please enter email address",
       },
-
       {
         type: "input",
         name: "github",
         message: "Please enter your GitHub username",
+      },
+      {
+        type:"input",
+        name: "filename",
+        message: "Please enter filename",
       }
 ];
 
-function promptUser() {
-    return inquirer.prompt(questions);
-  }
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function (err) {
-        if (err) {
-          console.error(err);
-        }
-      });
-}
+  fs.appendFile(`${fileName}.md`, generateMarkdown(data), (err) =>
+  err ? console.log(err) : console.log(`${fileName}.md was successfully created`)
+  )};
 
 // TODO: Create a function to initialize app
-async function init() {
-    console.log("Professional README Generator");
-    try {
-      const answers = await promptUser();
-      const readMe = generateMarkdown(answers);
-      writeToFile("newREADME.md", readMe);
-      console.log("new README.md created!");
-    } catch (err) {
-      console.log(err);
-    }
-  }
+function init() {
+  inquirer 
+  .prompt(questions)
+  .then((response) => {
+      writeToFile(response.fileName, response);
+  })
+  .catch(err => {
+      console.log(err)
+  });
+}
 // Function call to initialize app
 init();
